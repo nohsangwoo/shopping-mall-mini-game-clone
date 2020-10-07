444a17704a99867d39176d0ecdf6ae9975a75881
nohsangwoo <nsgr12@naver.com>
2020-10-07 17:41:43 +0900

    handling evets

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ function loadItems() {
 
 function displayItems(items) {
   const container = document.querySelector(".items");
-
   container.innerHTML = items.map((item) => createHTMLString(item)).join("");
 }
 
@@ -19,11 +18,32 @@ function createHTMLString(item) {
   </li>
   `;
 }
+
+function onButtonClick(event, items) {
+  const {
+    target: {
+      dataset: { key, value },
+    },
+  } = event;
+
+  if (key === (null || undefined) || value === (null || undefined)) {
+    return;
+  }
+  displayItems(items?.filter((item) => item[key] === value));
+}
+
+function setEventListensers(items) {
+  const logo = document.querySelector(".logo");
+  const buttons = document.querySelector(".buttons");
+  logo.addEventListener("click", () => displayItems(items));
+  buttons.addEventListener("click", (event) => onButtonClick(event, items));
+}
+
 // main
 loadItems()
   .then((items) => {
     console.log(items);
     displayItems(items);
-    // setEventListensers(items);
+    setEventListensers(items);
   })
   .catch(console.log);
