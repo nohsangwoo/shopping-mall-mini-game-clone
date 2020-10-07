c2e2b64d66a84ff8df8f8006290ecaca1f32b7da
nohsangwoo <nsgr12@naver.com>
2020-10-07 17:14:36 +0900

    spread json

--- src/main.js
+++ src/main.js
@@ -5,11 +5,25 @@ function loadItems() {
     .then((json) => json.items);
 }
 
+function displayItems(items) {
+  const container = document.querySelector(".items");
+
+  container.innerHTML = items.map((item) => createHTMLString(item)).join("");
+}
+
+function createHTMLString(item) {
+  return `
+  <li class="item">
+    <img src="${item.image}" alt="${item.type}" class="utem__thumbnail" />
+    <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
 // main
 loadItems()
   .then((items) => {
     console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListensers(items);
   })
   .catch(console.log);
